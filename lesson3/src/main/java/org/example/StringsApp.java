fded328d56c0d5dd86e7ffa21d1c9d36ae72682f
telman03 <telmangadimov1@gmail.com>
2022-07-03 12:59:47 +0400

    lesson3

--- lesson3/src/main/java/org/example/StringsApp.java
+++ lesson3/src/main/java/org/example/StringsApp.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.Locale;
 
 public class StringsApp {
     public static void main(String[] args) {
@@ -10,16 +9,23 @@ public class StringsApp {
 
         String str1 = "Ibatech";
         String str2 = "Ibatech";
-//        String str3 = new String("Ibatech");
-//        String str4 = new String("Ibatech");
-//        System.out.println(str1 == str2); // true
-//        System.out.println(str2 == str3); //false
-//        System.out.println(str1.equals(str3));  // true
-//        System.out.println(str3 == str4); //false
+        String str3 = new String("Ibatech");
+        String str4 = new String("Ibatech");
+        System.out.println(str1 == str2); // true
+        System.out.println(str2 == str3); //false
+        System.out.println(str1.equals(str3));  // true
+        System.out.println(str3 == str4); //false
 
-//        System.out.println(str3 + "or");
+        System.out.println(str3 + "or");
 
         System.out.println(str1);
 
+        StringBuffer sBuff = new StringBuffer("String");
+        StringBuilder sBuild = new StringBuilder();
+
+        sBuff.append("val");
+        System.out.println(sBuff);
+
+        System.out.print(String.format("%12.12f", 32.45));
     }
 }
