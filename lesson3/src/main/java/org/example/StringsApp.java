eafb9c86bed0b885757540c959724fab9c79a4e7
telman03 <telmangadimov1@gmail.com>
2022-07-03 12:37:04 +0400

    lesson3

---
+++ lesson3/src/main/java/org/example/StringsApp.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Locale;
+
+public class StringsApp {
+    public static void main(String[] args) {
+//        char[] chars = {'S', 't', 'r'};
+//        String str = new String(chars);
+//        System.out.println(str);
+
+        String str1 = "Ibatech";
+        String str2 = "Ibatech";
+//        String str3 = new String("Ibatech");
+//        String str4 = new String("Ibatech");
+//        System.out.println(str1 == str2); // true
+//        System.out.println(str2 == str3); //false
+//        System.out.println(str1.equals(str3));  // true
+//        System.out.println(str3 == str4); //false
+
+//        System.out.println(str3 + "or");
+
+        System.out.println(str1);
+
+    }
+}
