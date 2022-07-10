022acd753c459d99fe80b8a68ac3c56802c3c9a4
telman03 <telmangadimov1@gmail.com>
2022-07-10 20:08:35 +0400

    lesson4

--- lesson4/src/main/java/org/example/Envelope.java
+++ lesson4/src/main/java/org/example/Envelope.java
@@ -2,8 +2,9 @@ package org.example;
 
 public class Envelope {
     public static void main(String[] args) {
-        int height = 8;
-        int width = 20;
+        int height = 10;
+        int width = 25;
+        double tan = (double) width / height;
 
         final int firstRow = 0;
         final int firstCol = 0;
@@ -12,7 +13,10 @@ public class Envelope {
 
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                if(row == firstRow ||
+                if(
+                        row == firstRow ||
+                        row *tan== col ||
+                        row*tan + col == width - 1 ||
                         col == firstCol ||
                         col == lastCol ||
                         row == lastRow)
