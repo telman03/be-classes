711be3f3772245e21144db5781a901d3bb79fecd
telman03 <telmangadimov1@gmail.com>
2022-08-02 20:27:18 +0400

    Added version 2

--- lesson-9/src/main/java/task/DiagonalDifferenceTest.java
+++ lesson-9/src/main/java/task/DiagonalDifferenceTest.java
@@ -1,24 +1,35 @@
 package task;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DiagonalDifferenceTest {
+
+    private DiagonalDifferenceV2 dd = new DiagonalDifferenceV2();
+    @Test
     public void test1() {
-//        int[][] data0 = {
-//                {1,2,3},
-//                {4,5,6},
-//                {7,8,9}
-//        };
-//        int[] ints = data0[0];
-
-
-
         List<Integer> list1 = Arrays.asList(1, 2, 3);
         List<Integer> list2 = Arrays.asList(4, 5, 6);
-        List<Integer> list3 = Arrays.asList(7, 8, 9);
-        List<List<Integer>> data = Arrays.
-                asList(list1, list2, list3);
+        List<Integer> list3 = Arrays.asList(17, 8, 9);
+        List<List<Integer>> data = Arrays.asList(list1, list2, list3);
+        int real = dd.diagonalDifference(data);
+        int expected = 10;
+        assertEquals(expected, real);
+    }
+    @Test
+    public void test2() {
+        List<List<Integer>> data = Arrays.asList(
+                Arrays.asList(11, 2, 4),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(10, 8, -12)
+        );
 
+        int real = dd.diagonalDifference(data);
+        int expected = 15;
+        assertEquals(expected, real);
     }
 }
---
+++ lesson-9/src/main/java/task/DiagonalDifferenceV1.java
@@ -0,0 +1,26 @@
+package task;
+
+import java.util.List;
+
+public class DiagonalDifferenceV1 {
+    public int diagonalDifference(List<List<Integer>> data) {
+
+        int sum1 = 0;
+        int size = data.size();
+        // diagonal 1
+        for (int i = 0; i < size; i++) {
+            sum1 += data.get(i).get(i);
+        }
+
+        // diagonal 2
+        int sum2 = 0;
+        for (int i = 0; i < size; i++) {
+            sum2 += data.get(i).get(size - i - 1);
+        }
+
+        List<Integer> row1 = data.get(0);
+        Integer itemAt00 = row1.get(0);
+
+        return Math.abs(sum1 - sum2);
+    }
+}
