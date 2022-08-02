6c99c5d5abb41b4d9f0d6b13458230bdf415a205
telman03 <telmangadimov1@gmail.com>
2022-08-02 15:14:23 +0400

    update

---
+++ lesson-9/src/main/java/task/DiagonalDifference.java
@@ -0,0 +1,15 @@
+package task;
+
+import java.util.List;
+
+public class DiagonalDifference {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int sum1 = 0;
+
+
+
+        int sum2 = 0;
+        return Math.abs(sum1 - sum2);
+    }
+}
---
+++ lesson-9/src/main/java/task/DiagonalDifferenceTest.java
@@ -0,0 +1,14 @@
+package task;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DiagonalDifferenceTest {
+    public void test1(){
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = Arrays.asList(4, 5, 6);
+        List<Integer> list3 = Arrays.asList(7, 8, 9);
+        List<List<Integer>> data = Arrays.asList(list1, list2, list3);
+
+    }
+}
