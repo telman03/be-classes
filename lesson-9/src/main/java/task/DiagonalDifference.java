3f0d26585f455b9bce25ed12f8008142dd98e8b7
telman03 <telmangadimov1@gmail.com>
2022-08-02 19:16:09 +0400

    update

--- lesson-9/src/main/java/task/DiagonalDifference.java
+++ lesson-9/src/main/java/task/DiagonalDifference.java
@@ -3,11 +3,17 @@ package task;
 import java.util.List;
 
 public class DiagonalDifference {
-    public static int diagonalDifference(List<List<Integer>> arr) {
+    public static int diagonalDifference(List<List<Integer>> data) {
         // Write your code here
         int sum1 = 0;
+        int size = data.size();
 
+        for (int i = 0; i < size; i++) {
+            
+        }
 
+        List<Integer> row1 = data.get(0);
+        Integer itemAt00 = row1.get(0);
 
         int sum2 = 0;
         return Math.abs(sum1 - sum2);
--- lesson-9/src/main/java/task/DiagonalDifferenceTest.java
+++ lesson-9/src/main/java/task/DiagonalDifferenceTest.java
@@ -4,11 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DiagonalDifferenceTest {
-    public void test1(){
+    public void test1() {
+//        int[][] data0 = {
+//                {1,2,3},
+//                {4,5,6},
+//                {7,8,9}
+//        };
+//        int[] ints = data0[0];
+
+
+
         List<Integer> list1 = Arrays.asList(1, 2, 3);
         List<Integer> list2 = Arrays.asList(4, 5, 6);
         List<Integer> list3 = Arrays.asList(7, 8, 9);
-        List<List<Integer>> data = Arrays.asList(list1, list2, list3);
+        List<List<Integer>> data = Arrays.
+                asList(list1, list2, list3);
 
     }
 }
