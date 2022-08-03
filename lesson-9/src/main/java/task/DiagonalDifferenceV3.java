b5c2da545aecbf5d980f9f4ad72dd556e4d40789
telman03 <telmangadimov1@gmail.com>
2022-08-03 10:19:28 +0400

    Added version 3

---
+++ lesson-9/src/main/java/task/DiagonalDifferenceV3.java
@@ -0,0 +1,22 @@
+package task;
+
+import java.util.List;
+
+public class DiagonalDifferenceV3 {
+    public int diagonalDifference(List<List<Integer>> data) {
+        int size = data.size();
+
+        // diagonal 1
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            List<Integer> row = data.get(i);
+            sum = sum + row.get(i) - row.get(size - i - 1);
+        }
+
+
+        List<Integer> row1 = data.get(0);
+        Integer itemAt00 = row1.get(0);
+
+        return Math.abs(sum);
+    }
+}
