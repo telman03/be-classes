febbb276c6f4bbe62f37c1306e91be0ae64db621
telman03 <telmangadimov1@gmail.com>
2022-08-07 10:33:17 +0400

    lesson12

---
+++ lesson-12/src/main/java/task/BracketsApp.java
@@ -0,0 +1,34 @@
+package task;
+
+/**
+ * {
+ *   {
+ *     <<>><>
+ *     {}{}(
+ *     {
+ *       ()
+ *     }
+ *     )
+ *   }
+ *   [][()]
+ *   ()
+ * }
+ *
+ * 1. {}
+ * 2. []
+ * 3. ()
+ * 4. <>
+ *
+ */
+public class BracketsApp {
+
+    public boolean isValid(String line) {
+        throw new IllegalStateException("should be implemented");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
