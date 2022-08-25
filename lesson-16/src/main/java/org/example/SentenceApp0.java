5731f4fe819a571ad599ba58b31830f2098607e8
telman03 <telmangadimov1@gmail.com>
2022-08-25 20:27:56 +0400

    lesson16

--- lesson-16/src/main/java/org/example/SentenceApp0.java
+++ lesson-16/src/main/java/org/example/SentenceApp0.java
@@ -2,6 +2,46 @@ package org.example;
 
 import java.util.List;
 
+/**------------
+ |  Given:  |
+ ------------
+ List<String> subjects = list("Noel", "The cat", "The dog")
+ List<String> verbs =    list("wrote", "chased", "slept on")
+ List<String> objects =  list("the book","the ball","the bed")
+ List<String> sentences = all_possible(subjects, verbs, objects)
+ ------------
+ |  Goal:   |
+ ------------
+ To build all possible combinations of these words:
+ Noel wrote the book
+ Noel wrote the ball
+ Noel wrote the bed
+ Noel chased the book
+ Noel chased the ball
+ Noel chased the bed
+ Noel slept on the book
+ Noel slept on the ball
+ Noel slept on the bed
+ The cat wrote the book
+ The cat wrote the ball
+ The cat wrote the bed
+ The cat chased the book
+ The cat chased the ball
+ The cat chased the bed
+ The cat slept on the book
+ The cat slept on the ball
+ The cat slept on the bed
+ The dog wrote the book
+ The dog wrote the ball
+ The dog wrote the bed
+ The dog chased the book
+ The dog chased the ball
+ The dog chased the bed
+ The dog slept on the book
+ The dog slept on the ball
+ The dog slept on the bed
+ */
+
 public class SentenceApp0 {
     public static void main(String[] args) {
         List<String> subjects = List.of("Noel", "The cat", "The dog");
