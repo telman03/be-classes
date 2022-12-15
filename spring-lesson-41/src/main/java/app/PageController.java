11bef6945bafcdf067e0a9a0b8355e28a2f440f0
telman03 <telmangadimov1@gmail.com>
2022-12-15 22:55:06 +0400

    lesson41

---
+++ spring-lesson-41/src/main/java/app/PageController.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("page")
+    public String page_handle(Model model){
+        return "page01";
+    }
+}
---
+++ spring-lesson-41/src/main/java/app/model/Line.java
@@ -0,0 +1,11 @@
+package app.model;
+
+public class Line {
+    String name;
+    Double price;
+
+    public Line(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
