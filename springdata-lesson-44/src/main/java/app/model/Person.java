15101f9e12e22c90839c6889edd3bd749eca8c87
telman03 <telmangadimov1@gmail.com>
2023-01-01 21:55:57 +0400

    lesson 44

--- springdata-lesson-44/src/main/java/app/model/Person.java
+++ springdata-lesson-44/src/main/java/app/model/Person.java
@@ -4,16 +4,15 @@ package app.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 @Entity
+
+@Table(name = "person")
 public class Person {
 
     @Id
