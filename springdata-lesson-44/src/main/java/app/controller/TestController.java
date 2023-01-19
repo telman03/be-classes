3e58a8c3454b9381d7f165f762d0e4d762c971ba
telman03 <telmangadimov1@gmail.com>
2023-01-19 18:05:55 +0400

    spring data 2.1

--- springdata-lesson-44/src/main/java/app/controller/TestController.java
+++ springdata-lesson-44/src/main/java/app/controller/TestController.java
@@ -5,9 +5,13 @@ import app.model.Person;
 import app.model.PersonCreateRq;
 import app.repository.PersonRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("person")
@@ -18,14 +22,34 @@ public class TestController {
 
     @GetMapping
     public List<Person>  handle1(){
-        return repo.findAll();
+        List<Person> data = repo.findAll();
+        return data;
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Person> handle2(@PathVariable Integer id){
+        Optional<Person> data = repo.findById(id);
+        return ResponseEntity.of(data);
     }
 
+
     @PostMapping
-    public void handle2(@RequestBody PersonCreateRq rq){
+    public void handle3(@RequestBody PersonCreateRq rq){
         Person p = new Person();
         p.setUsername(rq.getUsername());
         p.setPassword(rq.getPassword());
         repo.save(p);
+    }
+
+
+
+    public void test(){
+        Optional<Person> byId = repo.findById(123);
+
+        /** ANTI-PATTERN !!! it fetches All the reloads */
+        List<Person> all = repo.findAll();
+
+
+        Pageable pageable = Pageable.ofSize(20).withPage(5);
+        Page<Person> page = repo.findAll(pageable);
     }
 }
