901586b1d296361a237dc9e320e03b9dddb76b88
telman03 <telmangadimov1@gmail.com>
2023-01-19 17:14:46 +0400

    spring data 1

--- springdata-lesson-44/src/main/java/app/repository/PersonRepo.java
+++ springdata-lesson-44/src/main/java/app/repository/PersonRepo.java
@@ -5,8 +5,7 @@ import app.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PersonRepo extends JpaRepository<Person, Integer> {
-
-
 }
