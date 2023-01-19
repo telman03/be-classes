fc558e3c0cd8ff8b0f483dd9142f3fb178a0650d
telman03 <telmangadimov1@gmail.com>
2023-01-19 17:31:10 +0400

    spring data 2

--- springdata-lesson-44/src/main/java/app/model/Person.java
+++ springdata-lesson-44/src/main/java/app/model/Person.java
@@ -13,13 +13,17 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 
-@Table(name = "person")
+//@Table(name = "person1") //table name
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id1")
     private Integer id;
+//    @Column(name = "username1")
     private String username;
+
+//    @Column(name = "password1")
     private String password;
 
 }
