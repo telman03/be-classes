5615a2555d45be2f8ee3d61cead8dd21942e1817
telman03 <telmangadimov1@gmail.com>
2023-01-27 12:41:27 +0400

    spring security users fetched every time on login

--- spring-lesson-49/src/main/java/app/security/UserDetailsServiceJPA.java
+++ spring-lesson-49/src/main/java/app/security/UserDetailsServiceJPA.java
@@ -2,25 +2,28 @@ package app.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
-
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class UserDetailsServiceJPA  implements UserDetailsService {
 
     private final DbUserRepo repo;
 
-    public static UserDetails mapper(DbUser dbUser){
+    public static UserDetails mapper(DbUser dbUser) {
         return User
                 .withUsername(dbUser.getUsername())
-                .password(dbUser.getPassword()) // password already stored in db encoded
+                .password(dbUser.getPassword())
+                // password stored in the DB already encoded, we don't need to deal with encoding
                 .roles()
                 .build();
     }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
