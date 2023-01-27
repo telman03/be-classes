53464e0e4b65e0490482c2b9ecb44afd11eacff2
telman03 <telmangadimov1@gmail.com>
2023-01-27 12:31:14 +0400

    spring security

---
+++ spring-lesson-49/src/main/java/app/security/UserDetailsServiceJPA.java
@@ -0,0 +1,33 @@
+package app.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class UserDetailsServiceJPA  implements UserDetailsService {
+
+    private final DbUserRepo repo;
+
+    public static UserDetails mapper(DbUser dbUser){
+        return User
+                .withUsername(dbUser.getUsername())
+                .password(dbUser.getPassword()) // password already stored in db encoded
+                .roles()
+                .build();
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return repo.findByUsername(username)
+                .map(UserDetailsServiceJPA::mapper)
+                .orElseThrow(() ->new UsernameNotFoundException(
+                        String.format("User '%s' not found", username)
+                ));
+    }
+}
