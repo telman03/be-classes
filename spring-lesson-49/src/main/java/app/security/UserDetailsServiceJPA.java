99fa6706e8ae0105bacf78d8c37bd724e1d180e1
telman03 <telmangadimov1@gmail.com>
2023-01-27 12:43:30 +0400

    spring security users fetched every time on login

--- spring-lesson-49/src/main/java/app/security/UserDetailsServiceJPA.java
+++ spring-lesson-49/src/main/java/app/security/UserDetailsServiceJPA.java
@@ -1,41 +1,19 @@
 package app.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
-import org.springframework.security.core.userdetails.UserDetails;
-
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
-=======
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-
-@Configuration
->>>>>>> origin/main
 @RequiredArgsConstructor
 public class UserDetailsServiceJPA  implements UserDetailsService {
 
     private final DbUserRepo repo;
 
-<<<<<<< HEAD
-    public static UserDetails mapper(DbUser dbUser) {
-        return User
-                .withUsername(dbUser.getUsername())
-                .password(dbUser.getPassword())
-                // password stored in the DB already encoded, we don't need to deal with encoding
-                .roles()
-                .build();
-    }
 
-=======
     public static UserDetails mapper(DbUser dbUser){
         return User
                 .withUsername(dbUser.getUsername())
@@ -43,7 +21,6 @@ public class UserDetailsServiceJPA  implements UserDetailsService {
                 .roles()
                 .build();
     }
->>>>>>> origin/main
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
